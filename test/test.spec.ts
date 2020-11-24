f0d3313f2945969981da999b82f653a3264551b1
soltisivan <soltisivan11@gmail.com>
2020-11-24 21:51:25 +0200

    add tests mocha

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,13 +1,14 @@
-import { SqlReader } from "../src";
 import * as path from "path";
+import { SqlReader } from "../src";
+import { assert } from "chai";
 
 describe('Test', () => {
 
     it('should read-sql', () => {
         console.log('Hello world')
-        // let queries = SqlReader.readSqlFile(path.join(__dirname, "../data/users.sql"));
-        // console.log(queries)
-        // expect(queries).toHaveSize(3)
+        let queries = SqlReader.readSqlFile(path.join(__dirname, "../data/users.sql"));
+        console.log(queries)
+        assert.lengthOf(queries, 3)
     });
 
 
