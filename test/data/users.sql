aecbf2a22ecfe6f1a66417534b5245dc0806630e
soltisivan <soltisivan11@gmail.com>
2020-11-24 21:20:10 +0200

    jasmin tests, add test data

---
+++ test/data/users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (email, password, name) VALUES ('dummy1@somemail.com', 'hashed_password1', 'dummy1');
+INSERT INTO users (email, password, name) VALUES ('dummy2@somemail.com', 'hashed_password2', 'dummy2');
+INSERT INTO users (email, password, name) VALUES ('dummy3@somemail.com', 'hashed_password3', 'dummy3');
---
+++ test/test.spec.ts
@@ -0,0 +1,15 @@
+import { SqlReader } from "../src";
+import * as path from "path";
+
+describe('Test', () => {
+
+    it('should read-sql', () => {
+        console.log('Hello world')
+        // let queries = SqlReader.readSqlFile(path.join(__dirname, "../data/users.sql"));
+        // console.log(queries)
+        // expect(queries).toHaveSize(3)
+    });
+
+
+
+})
